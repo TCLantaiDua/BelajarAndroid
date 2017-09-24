1eac88355a1514ac28bed9fd3fb6a4326befc466
jegunk <agungnugroho94@gmail.com>
2017-09-24 17:03:08 +0700

    ArrayList tanpa bisa diklik

---
+++ app/src/main/java/id/toha/itdept/belajar/Main3Activity.java
@@ -0,0 +1,45 @@
+package id.toha.itdept.belajar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        ArrayList<String> words = new ArrayList<String>();
+
+        words.add("satu");
+        words.add("dua");
+        words.add("tiga");
+        words.add("empat");
+        words.add("lima");
+        words.add("enam");
+        words.add("tujuh");
+        words.add("delapan");
+
+
+
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+        int index = 0;
+        while (index<words.size()){
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+            index++;
+        }
+
+
+
+    }
+}
