3175f1ad8973111070a0162b1f2443436b095cb6
Toha Abdurrozak <tohaabdur@gmail.com>
2017-09-24 15:47:08 +0700

    Merubah manifest supaya activity ListViewActivity menjadi Launcher

--- app/src/main/java/id/toha/itdept/belajar/ListViewActivity.java
+++ app/src/main/java/id/toha/itdept/belajar/ListViewActivity.java
@@ -2,12 +2,43 @@ package id.toha.itdept.belajar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class ListViewActivity extends AppCompatActivity {
+public class ListViewActivity extends AppCompatActivity
+{
+
+    //Deklarasi object Listview dan String Array
+    private ListView ListViewData;
+    private String[] Data = new String[]{"Pisang","Apel","Anggur","Pepaya","Mangga","Mangga 1","Mangga 2","Mangga 3","Mangga 4","Mangga 5","Mangga 6","Mangga 1","Mangga 2","Mangga 3","Mangga 4","Mangga 5","Mangga 6"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+
+        ListViewData = (ListView) findViewById(R.id.ListViewData);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                ListViewActivity.this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                Data
+                );
+
+        ListViewData.setAdapter(adapter);
+
+        ListViewData.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                Toast.makeText(getApplicationContext(), Data[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
